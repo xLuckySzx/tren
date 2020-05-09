9c969cc8c8f4e3e179235bf4fedc16ef2abb982d
Luca Ferrandini <lcferrandini@gmail.com>
2020-05-09 10:06:43 +0200

    Removed mistaken /register route

--- backend/routes/food.js
+++ backend/routes/food.js
@@ -1,6 +1,5 @@
 const passport = require('passport')
 const router = require('express').Router();
-const User = require('../models/user')
 const Food = require('../models/food')
 
 router.all('/*', passport.authenticate('jwt', { session: false }))
@@ -52,41 +51,6 @@ router.post('/add', (req, res) => {
             message: err
         })
     })
-})
-
-
-
-router.post('/register', (req, res, next) => {
-    passport.authenticate('register', (err, user, info) => {
-        if(err) {
-            console.log(err)
-        }
-        if(info !== undefined) {
-            console.log(info.message)
-            res.send(info.message)
-        } else {
-            req.logIn(user, err => {
-                const data = {
-                    email: req.body.email,
-                    bio: req.body.bio
-                }
-
-                User.findOne({ username: user.username })
-                    .then(user => {
-                        user.updateOne({
-                            // Add additional information
-                            // TODO: add image
-                            email: data.email,
-                            bio: data.bio
-                        })
-                        .then(() => {
-                            console.log('user created')
-                            res.status(200).send({ message: 'registration successful' })
-                        })
-                    })
-            })
-        }
-    })(req, res, next)
 })
 
 module.exports = router
